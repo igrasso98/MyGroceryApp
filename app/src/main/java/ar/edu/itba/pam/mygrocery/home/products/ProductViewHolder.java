d87ca06d5cd002e272f3b52886ba33abec65ef3a
nicolas <nicbritos@hotmail.com>
2021-11-10 07:09:40 -0300

    fix default market in add product to list dialog

--- app/src/main/java/ar/edu/itba/pam/mygrocery/home/products/ProductViewHolder.java
+++ app/src/main/java/ar/edu/itba/pam/mygrocery/home/products/ProductViewHolder.java
@@ -37,7 +37,7 @@ public class ProductViewHolder extends RecyclerView.ViewHolder {
         final ImageView addProductButton = itemView.findViewById(R.id.shop_product);
         addProductButton.setOnClickListener(v -> {
             if (listener != null) {
-                bindMarketDialog(itemView.getContext(), model.getId(), markets);
+                bindMarketDialog(itemView.getContext(), model.getId(), model.getMarketId(), markets);
             }
         });
     }
@@ -46,16 +46,18 @@ public class ProductViewHolder extends RecyclerView.ViewHolder {
         this.listener = listener;
     }
 
-    private void bindMarketDialog(final Context context, final Long productId, final List<Market> markets) {
+    private void bindMarketDialog(final Context context, final Long productId, final Long marketId, final List<Market> markets) {
 
         AlertDialog.Builder myDialog = new AlertDialog.Builder(context);
         myDialog.setTitle("Comprar producto");
         myDialog.setMessage("Seleccione a que lista se agregará el producto.");
+
         final Spinner marketsSpinner = new Spinner(context);
         ArrayAdapter<Market> marketArrayAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, markets);
         marketArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         marketsSpinner.setAdapter(marketArrayAdapter);
         marketsSpinner.setPadding(toDp(context,16),0,toDp(context,16),0);
+        marketsSpinner.setSelection(getMarketPosition(markets, marketId));
         myDialog.setView(marketsSpinner);
 
         myDialog.setPositiveButton("Comprar", (dialogInterface, i) -> AsyncTask.execute(() -> listener.onBuyProductClicked(productId, ((Market) marketsSpinner.getSelectedItem()).getId())));
@@ -66,5 +68,17 @@ public class ProductViewHolder extends RecyclerView.ViewHolder {
     // Extension method to convert pixels to dp
     private int toDp(Context context, int value) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, Long.valueOf(value), context.getResources().getDisplayMetrics());
+    }
+
+    private int getMarketPosition(List<Market> markets, Long marketId) {
+        Integer foundIndex = null;
+        for (int i = 0; i < markets.size(); i++) {
+            if (markets.get(i).getId().equals(marketId)) {
+                foundIndex = i;
+                break;
+            }
+        }
+        assert foundIndex != null;
+        return foundIndex;
     }
 }
